387fd98dd35e03cb3f03e11e0e17f88cd85008ed
Sam Brez <sambreslin02@gmail.com>
2024-03-02 13:22:46 -0500

    contact and resume page basics

--- react-portfolio/src/pages/Contact.jsx
+++ react-portfolio/src/pages/Contact.jsx
@@ -1,9 +1,17 @@
 function Contact () {
     return (
         <>
-        <h1>
-            TEST 2
-        </h1>
+        <h2>Reach Out!</h2>
+        <form>
+            <label>NAME:</label>
+            <input type='text' name='name' required />
+            <label>EMAIL:</label>
+            <input type='email' name='email' required />
+            <label>MESSAGE:</label>
+            <textarea name='message' rows='3' required></textarea>
+            <br/>
+            <button type='submit'>Submit</button>
+        </form>
         </>
     )
 }
--- react-portfolio/src/pages/Resume.jsx
+++ react-portfolio/src/pages/Resume.jsx
@@ -1,11 +1,55 @@
-function Resume () {
-    return (
-        <>
-        <h1>
-            TEST 4
-        </h1>
-        </>
-    )
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Resume() {
+  return (
+    <>
+      <h2>
+        Download my{" "}
+        <a
+          href="https://drive.google.com/file/d/1cffRW8koTycghkFh3qqQ9Ietz7TTk5hP/view?usp=sharing"
+          download
+        >
+          resume!
+        </a>
+      </h2>
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">Front-End Proficiencies</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row"></th>
+            <td>HTML</td>
+            <td>CSS</td>
+            <td>JavaScript</td>
+            <td>JQuery</td>
+            <td>BootStrap</td>
+            <td>React</td>
+          </tr>
+        </tbody>
+      </table>
+      <br/>
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">Back-End Proficiencies</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row"></th>
+            <td>APIs</td>
+            <td>Node.js</td>
+            <td>Express.js</td>
+            <td>MySQL & Sequelize</td>
+            <td>MongoDB & Mongoose</td>
+          </tr>
+        </tbody>
+      </table>
+    </>
+  );
 }
 
 export default Resume;
