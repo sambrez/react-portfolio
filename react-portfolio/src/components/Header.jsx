d0183f1102ec399da52f4370e301a3c4a5c0b17c
Sam Brez <sambreslin02@gmail.com>
2024-04-04 12:08:46 -0400

    responsive navigation updates

--- react-portfolio/src/components/Header.jsx
+++ react-portfolio/src/components/Header.jsx
@@ -1,17 +1,28 @@
-import Navigation from './Navigation';
+import Navigation from "./Navigation";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../styles/Header.css"; 
 
-function Header () {
-    return (
-       <div id="top" className='container-fluid'>
-       <header className='row navbar-header'>
-            <img id="laptop" src="../../img/laptop.svg" />
-            <h1 id='title-one' className="col">SAM BRESLIN</h1>
-            <Navigation />
-        </header>
-        </div> 
-    )
+function Header() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+      <h1 className="navbar-brand" id="title">SAM BRESLIN</h1>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse justify-content-end ml-40" id="navbarNav">
+        <Navigation />
+      </div>
+    </nav>
+  );
 }
 
 export default Header;
--- react-portfolio/src/components/Navigation.jsx
+++ react-portfolio/src/components/Navigation.jsx
@@ -1,57 +1,48 @@
 import { Link, useLocation } from "react-router-dom";
-
-
-import "bootstrap/dist/css/bootstrap.min.css";
-import "../styles/Navigation.css";
+import "../styles/Navigation.css"; 
 
 function Navigation() {
   const currentPage = useLocation().pathname;
 
   return (
-    <nav className="col">
-      <ul className="row nav nav-tabs" id="navigation">
-        <li className="col-lg nav-item" id="nav-one" >
-          <Link
-            to="/About"
-            className={
-              currentPage === "/About" ? "nav-link active" : "nav-link"
-            }
-          >
-            About Me
-          </Link>
-        </li>
-        <li className="col-lg" id="nav-two">
-          <Link
-            to="/Portfolio"
-            className={
-              currentPage === "/Portfolio" ? "nav-link active" : "nav-link"
-            }
-          >
-            Portfolio
-          </Link>
-        </li>
-        <li className="col-lg" id="nav-three">
-          <Link
-            to="/Resume"
-            className={
-              currentPage === "/Resume" ? "nav-link active" : "nav-link"
-            }
-          >
-            Resume
-          </Link>
-        </li>
-        <li className="col-lg" id="nav-four">
-          <Link
-            to="/Contact"
-            className={
-              currentPage === "/Contact" ? "nav-link active" : "nav-link"
-            }
-          >
-            Contact
-          </Link>
-        </li>
-      </ul>
-    </nav>
+    <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li className="nav-item">
+        <Link
+          to="/About"
+          className={currentPage === "/About" ? "nav-link active" : "nav-link"}
+        >
+          About Me
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          to="/Portfolio"
+          className={
+            currentPage === "/Portfolio" ? "nav-link active" : "nav-link"
+          }
+        >
+          Portfolio
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          to="/Resume"
+          className={currentPage === "/Resume" ? "nav-link active" : "nav-link"}
+        >
+          Resume
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          to="/Contact"
+          className={
+            currentPage === "/Contact" ? "nav-link active" : "nav-link"
+          }
+        >
+          Contact
+        </Link>
+      </li>
+    </ul>
   );
 }
 
