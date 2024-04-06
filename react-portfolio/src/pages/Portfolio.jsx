918f5d071732ec26c895638386ee8d91566d5b80
Sam Brez <sambreslin02@gmail.com>
2024-04-06 12:08:47 -0400

    updates to portfolio page

--- react-portfolio/src/pages/Portfolio.jsx
+++ react-portfolio/src/pages/Portfolio.jsx
@@ -1,10 +1,11 @@
 import Project from "../components/Project";
-import img1 from "../assets/tech-blog.png";
-import img2 from "../assets/cosmic.png";
-import img3 from "../assets/weather.png";
-import img4 from "../assets/note-taker.png";
-import img5 from "../assets/coding-quiz.png";
-import img6 from "../assets/literary.png"
+import img1 from "../assets/project-man.png";
+import img2 from "../assets/tech-blog.png";
+import img3 from "../assets/cosmic-counsel.png";
+import img4 from "../assets/weather-dash.png";
+import img5 from "../assets/note-taker.png";
+import img6 from "../assets/code-quiz.png";
+import img7 from "../assets/lit-voyagers.png"
 
 import "../styles/Portfolio.css";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -13,41 +14,48 @@ const projects = [
   {
     id: 1,
     image: img1,
+    title: "Project Management Tool",
+    link: "https://final-project-qomg.onrender.com",
+    repo: "https://github.com/sambrez/project-management",
+  },
+  {
+    id: 2,
+    image: img2,
     title: "Technology Blog",
     link: "https://techblog-mvc-14-85eba46978d7.herokuapp.com",
     repo: "https://github.com/sambrez/MVC-Tech-Blog",
   },
   {
-    id: 2,
-    image: img2,
+    id: 3,
+    image: img3,
     title: "Cosmic Counsel",
     link: "https://conartisttt.github.io/Cosmic-Counsel",
     repo: "https://github.com/sambrez/Cosmic-Counsel",
   },
   {
-    id: 3,
-    image: img3,
+    id: 4,
+    image: img4,
     title: "Weather Dashboard",
     link: "https://sambrez.github.io/weather-dashboard",
     repo: "https://github.com/sambrez/weather-dashboard",
   },
   {
-    id: 4,
-    image: img4,
+    id: 5,
+    image: img5,
     title: "Note Taker",
     link: "https://brez-note-taker-app-5ed47cdd234f.herokuapp.com",
     repo: "https://github.com/sambrez/express.js-note-taker-app",
   },
   {
-    id: 5,
-    image: img5,
+    id: 6,
+    image: img6,
     title: "Coding Quiz",
     link: "https://sambrez.github.io/coding-bootcamp-student-quiz",
     repo: "https://github.com/sambrez/coding-bootcamp-student-quiz",
   },
   {
-    id: 6,
-    image: img6,
+    id: 7,
+    image: img7,
     title: "Literary Voyagers",
     link: "https://literary-voyagers-5d41db9d0f86.herokuapp.com",
     repo: "https://github.com/sambrez/Literary-Voyagers",
